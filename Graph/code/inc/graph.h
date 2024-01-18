a9ade09780139c4ebea79ec9bfd45d497728e8c1
Mikhail <148915980+Kori-Tamashi@users.noreply.github.com>
2024-01-18 05:37:50 +0300

    Update graph.h

--- Graph/code/inc/graph.h
+++ Graph/code/inc/graph.h
@@ -11,9 +11,19 @@
 #define _STRING__ 256
 
 /**
+ * Forbidden characters for vertex name
+*/
+#define FORBIDDEN_SEPARATORS "\"\'#%()><{}-/\\|:;,"
+
+/**
  * \brief Positive return code
 */
 #define _GRAPH_OK__ 0  
+
+/**
+ * \brief Memory allocation error
+*/
+#define _GRAPH_MEM__ -1
 
 /**
  * \brief Incorrect arguments in function
@@ -37,16 +47,31 @@
 
 // Structs and functions
 
-struct edge;
+struct edge
+{
+    char start_vertex[_STRING__ + 1]; // name of start vertex
+    char end_vertex[_STRING__ + 1];   // name of end vertex
+    size_t length;                    // length of edge
+};
 
-struct graph;
+struct graph
+{
+    char **vertices;        // dynamic array of vertices names
+    size_t vertices_amount; // length of vertices array
+    struct edge *edges;     // dynamic array of edges
+    size_t edges_amount;    // length of edges array
+};
 
 typedef int graph_error_t;
+
+typedef struct graph graph_t;
 
 /**
  * \brief Initialization of graph by zero
  * 
  * \param[in] graph Graph descriptor
+ * 
+ * \note If the graph descriptor is NULL, the function will not cause a segmentation error
  */
 void graph_initialize(struct graph *graph);
 
@@ -56,6 +81,8 @@ void graph_initialize(struct graph *graph);
  * \param[in] graph Graph descriptor
  * 
  * \return `1` - `True` / `0` - `False`
+ * 
+ * \note If incorrect arguments are passed, the function returns `1` (`True`)
 */
 int graph_is_empty(struct graph *graph);
 
@@ -66,6 +93,8 @@ int graph_is_empty(struct graph *graph);
  * \param[in] vertex Vertex name
  * 
  * \return `1` - `True` / `0` - `False`
+ * 
+ * \note If incorrect arguments are passed, the function returns `0` (`False`)
 */
 int graph_has_vertex(struct graph *graph, const char *vertex);
 
@@ -77,6 +106,8 @@ int graph_has_vertex(struct graph *graph, const char *vertex);
  * \param[in] end_vertex End vertex name
  * 
  * \return `1` - `True` / `0` - `False`
+ * 
+ * \note If incorrect arguments are passed, the function returns `0` (`False`)
 */
 int graph_has_edge(struct graph *graph, const char *start_vertex, const char *end_vertex);
 
@@ -87,6 +118,10 @@ int graph_has_edge(struct graph *graph, const char *start_vertex, const char *en
  * \param[in] vertex Vertex name
  * 
  * \return `_GRAPH_OK__`, `_GRAPH_MEM__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EXIST__`
+ * 
+ * \note - You cannot add a copy of an existing vertex
+ * \note - You cannot add a vertex with a name of zero length
+ * \note - You cannot add a vertex with a name containing special characters - `#%()><{}-/\|:;,` and quotes
 */
 graph_error_t graph_add_vertex(struct graph *graph, const char *vertex);
 
@@ -96,7 +131,7 @@ graph_error_t graph_add_vertex(struct graph *graph, const char *vertex);
  * \param[in] graph Graph descriptor
  * \param[in] vertex Vertex name
  * 
- * \return `_GRAPH_OK__`, `_GRAPH_MEM__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EXIST__`
+ * \return `_GRAPH_OK__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EMPTY__`, `_GRAPH_NOT_FOUND__`
 */
 graph_error_t graph_delete_vertex(struct graph *graph, const char *vertex);
 
@@ -109,6 +144,8 @@ graph_error_t graph_delete_vertex(struct graph *graph, const char *vertex);
  * 
  * \return `_GRAPH_OK__`, `_GRAPH_MEM__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EXIST__`
  * 
+ * \note - You cannot add a copy of an existing edge
+ * \note - When adding an edge consisting of new vertices, new vertices will be added to the graph
 */
 graph_error_t graph_add_edge(struct graph *graph, const char *start_vertex, const char *end_vertex, size_t edge_length);
 
@@ -119,8 +156,15 @@ graph_error_t graph_add_edge(struct graph *graph, const char *start_vertex, cons
  * \param[in] start_vertex Start vertex name
  * \param[in] end_vertex End vertex name
  * 
- * \return `_GRAPH_OK__`, `_GRAPH_MEM__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EMPTY__`, `_GRAPH_NOT_FOUND__`
+ * \return `_GRAPH_OK__`, `_GRAPH_INCORRECT_ARG__`, `_GRAPH_EMPTY__`, `_GRAPH_NOT_FOUND__`
 */
 graph_error_t graph_delete_edge(struct graph *graph, const char *start_vertex, const char *end_vertex);
+
+/**
+ * \brief Free graph
+ * 
+ * \param[in] graph Graph descriptor
+*/
+void graph_free(struct graph *graph);
 
 #endif // GRAPH_H__
